4490b6a42611300d225388ace7409ae9b37e3b1e
jangdave <121008922+atg99@users.noreply.github.com>
2023-01-11 10:09:06 +0900

    23.01.11 0957
    create project

---
+++ Source/EldenRing/EldenRingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EldenRingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ELDENRING_API AEldenRingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
