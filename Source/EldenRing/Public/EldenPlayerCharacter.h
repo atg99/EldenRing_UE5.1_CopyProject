8d03a8ab70a9d47b2e89b4ffe4f51b0cbb8748e1
jangdave <121008922+atg99@users.noreply.github.com>
2023-01-11 15:18:08 +0900

    23 01 11 1517

---
+++ Source/EldenRing/Public/EldenPlayerCharacter.h
@@ -0,0 +1,69 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "EldenPlayerCharacter.generated.h"
+
+UCLASS()
+class ELDENRING_API AEldenPlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AEldenPlayerCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	class UBoxComponent* boxComp;
+
+	UPROPERTY(EditDefaultsOnly, Category = playerSettings)
+	class UInputAction* ia_Horizontal;
+
+	UPROPERTY(EditDefaultsOnly, Category = playerSettings)
+	class UInputAction* ia_Vertical;
+
+	UPROPERTY(EditDefaultsOnly, Category = playerSettings)
+	class UInputAction* ia_MoveForward;
+
+	UPROPERTY(EditDefaultsOnly, Category = playerSettings)
+	class UInputAction* ia_MoveRight;
+
+	UPROPERTY(EditDefaultsOnly, Category = playerSettings)
+	class UInputMappingContext* imc_myMapping;
+
+private:
+
+	UFUNCTION(BlueprintCallable)
+	void MoveForward(const FInputActionValue& val);
+
+	UFUNCTION(BlueprintCallable)
+	void MoveRight(const FInputActionValue& val);
+
+	UFUNCTION(BlueprintCallable)
+	void Horizontal(const FInputActionValue& val);
+
+	UFUNCTION(BlueprintCallable)
+	void Vertical(const FInputActionValue& val);
+
+	
+
+	UPROPERTY()
+	FVector direction;
+
+	float x;
+	float y;
+
+
+};
---
+++ Source/EldenRing/Public/MyEldenGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyEldenGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ELDENRING_API AMyEldenGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
